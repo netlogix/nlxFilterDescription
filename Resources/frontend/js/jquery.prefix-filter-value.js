637499d4bb5aa4be8aace96431de0ddaec9529ad
Vladislav Sultanov <sultanov@solutiondrive.de>
2019-05-20 14:01:49 +0200

    Add prefix to the active filter.

--- Resources/frontend/js/jquery.prefix-filter-value.js
+++ Resources/frontend/js/jquery.prefix-filter-value.js
@@ -1,34 +1,63 @@
 ;(function ($, window) {
     $.plugin('prefixFilterValue', {
-        //Die Idee war hier bei dem Event onCreateActiveFilter den Aktiven Filter Element zu erstellen.
         defaults: {
+            fieldName: '',
+
             currentForm: '',
+
+            filterFormSelector: '*[data-filter-form="true"]',
+
+            filterComponentSelector: '*[data-filter-type]',
+
+            // This is a temporary list and will be fill in the future by an ajax call or from a data attribute
+            filtersIdToBePrefix: ['bridge_width', 'temple_length', 'smallest_raw_glass_diameter'],
+
+            filtersName: {},
         },
 
         init: function () {
-            this.assignCurrentForm();
-            this.setFilterPrefix();
-            console.log(this.opts.currentForm, 'CurrentForm');
-            console.log($.trim(this.opts.currentForm), 'CurrentForm2');
+            var me = this;
+            me.$filterForm = $(me.opts.filterFormSelector);
+            me.$filterComponents = me.$filterForm.find(me.opts.filterComponentSelector);
+            me.assignFiltersName();
+            me.onUpdateActiveFilterElement();
+            me.onCreateActiveFilterElement();
         },
 
-        assignCurrentForm: function () {
+        assignFiltersName: function () {
             var me = this;
-            $.subscribe('plugin/swListingActions/onComponentChange', function (plugin, data, event) {
-                var fieldName = $(event.currentTarget).data('fieldName');
-                me.opts.currentForm = $.trim($('label[for='+  fieldName  +']').text());
-                console.log('first!');
+            $.each(me.$filterComponents, function (index, filterSelector) {
+                $.each(me.opts.filtersIdToBePrefix, function (index, filterId) {
+                    var filterData = $(filterSelector).data('field-name');
+                    if (filterData === filterId) {
+                        me.opts.filtersName[filterId] = $.trim($(filterSelector).find('label[for='+  filterId  +']').text());
+                    }
+                })
+
+            })
+        },
+
+        onUpdateActiveFilterElement: function () {
+            var me = this;
+            $.subscribe('plugin/swListingActions/onUpdateActiveFilterElement', function (plugin, data, param, label) {
+                me.setFilterPrefix(data, param, label);
             });
         },
 
-        //Die Idee war hier bei diesem Event den Aktiven Filter Element zu erstellen und die Filter Bezeichnung
-        // aus 'currentForm' zu benutzen. Leider funktioniert es nicht durch die Reihenfolge wie die aufgerufen werden.
-        setFilterPrefix: function () {
+        onCreateActiveFilterElement: function () {
             var me = this;
-            $.subscribe('plugin/swListingActions/onCreateActiveFilter', function (plugin, data, param, value) {
-                console.log('second!');
+            $.subscribe('plugin/swListingActions/onCreateActiveFilterElement', function (plugin, data, param, label) {
+                me.setFilterPrefix(data, param, label);
             });
-        }
+        },
+
+        setFilterPrefix: function (data, param, label) {
+            $.each(this.opts.filtersName, function (filterId, filterName) {
+                if (param.indexOf(filterId) >= 1) {
+                    data.activeFilterElements[param].html(data.getLabelIcon() + label + ' ' + filterName);
+                }
+            })
+        },
     });
     window.StateManager.addPlugin('body', 'prefixFilterValue');
 })(jQuery, window);
